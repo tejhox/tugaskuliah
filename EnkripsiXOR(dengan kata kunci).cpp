bbb4b77fc74bb6da083e4deae2939cf231cacae8
Bowo Dwi Laksono <121765657+tejhox@users.noreply.github.com>
2023-01-02 03:29:26 +0700

    Add files via upload

---
+++ EnkripsiXOR(dengan kata kunci).cpp
@@ -0,0 +1,36 @@
+// Enkripsi sembarang berkas dengan
+// algoritma XOR sederhana.
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+main(int argc, char *argv[])
+{
+  FILE *Fin, *Fout;
+  char p, c;
+  int i;
+  string K = "Saya harus lulus tepat waktu dan menjadi sarjana";
+
+  Fin = fopen(argv[1], "rb");
+  if (Fin == NULL) {
+  	cout << "Berkas " << argv[1] <<"tidak ada"<<endl;
+	exit(0); }
+	
+  Fout = fopen(argv[2], "wb"); 
+  cout << "Enkripsi " << argv[1] << "menjadi " << argv[2] << "...";
+  i=0;
+  while (!feof(Fin)) {
+  	p = getc(Fin);
+  	c = p ^ K[i]; // operasi XOR
+  	putc(c, Fout);
+  	i = (i + 1); K.length();
+	}
+  fclose (Fin);
+  fclose (Fout);
+}
+
+ 
+
+ 
